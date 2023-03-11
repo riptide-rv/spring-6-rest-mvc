d60895323179c5664888bf93159763f04d77f240
riptide-rv <rohitramdas2003@gmail.com>
2023-03-11 21:52:01 +0530

    working with spring mockito testing

---
+++ src/test/java/com/example/spring6restmvc/controllers/BeerControllerTest.java
@@ -0,0 +1,45 @@
+package com.example.spring6restmvc.controllers;
+
+import com.example.spring6restmvc.model.Beer;
+import com.example.spring6restmvc.services.BeerService;
+import com.example.spring6restmvc.services.BeerServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(BeerController.class)
+class BeerControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    BeerService beerService;
+
+    BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
+    @Test
+    void getBeerById() throws Exception {
+        Beer testBeer = beerServiceImpl.listBeers().get(0);
+        given(beerService.getBeerById(testBeer.getId())).willReturn(testBeer);
+
+        mockMvc.perform(get("/api/v1/beer/"+ testBeer.getId())
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id",is(testBeer.getId().toString())));
+
+    }
+}
